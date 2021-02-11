8a41e6b058c4d48f45658aa083637a7221fea952
dalinin-it <dalidim.it@gmail.com>
2021-02-11 15:36:15 +0300

    added Quasar to Laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .copy('darquest/dist/spa/index.html', 'resources/views/spa.blade.php')
+  .copyDirectory('darquest/dist/spa', 'public');
