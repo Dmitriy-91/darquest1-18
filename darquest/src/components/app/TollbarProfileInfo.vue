a93c24492ef0612e2e63b78ba831abdf8cce86cd
dalinin-it <dalidim.it@gmail.com>
2021-02-13 23:42:02 +0300

    init

---
+++ darquest/src/components/app/TollbarProfileInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <q-btn-dropdown
+        push
+        glossy
+        icon="login"
+        :label="$t('main.in_game')"
+        @click="showLoginForm"
+    >
+      <div>
+        <login></login>
+      </div>
+    </q-btn-dropdown>
+  </div>
+</template>
+
+<script>
+import Login from '../auth/Login'
+
+export default {
+  name: 'ToolbarProfileInfo',
+  components: { Login }
+}
+</script>
+
+<style scoped>
+
+</style>
