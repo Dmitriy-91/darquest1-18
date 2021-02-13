64529d0a5110a29fd81211e1a171b3a6be56f6d8
dalinin-it <dalidim.it@gmail.com>
2021-02-14 00:08:25 +0300

    init

---
+++ darquest/src/components/auth/Register.vue
@@ -0,0 +1,52 @@
+<template>
+  <div style="min-width: 300px">
+    <q-card>
+      <div class="row no-wrap q-px-md q-pt-md">
+        <div class="column">
+          <div class="text-h6 q-mb-md">{{ $t('main.sign_up') }}</div>
+        </div>
+      </div>
+      <q-card-section class="q-gutter-md">
+        <q-input
+            rounded
+            standout
+            v-model="login"
+            :label="$t('main.username')"
+        />
+        <q-input
+            rounded
+            standout
+            v-model="password"
+            :label="$t('main.password')"
+        />
+      </q-card-section>
+      <q-card-actions>
+        <q-btn
+            class="full-width"
+            push
+            glossy
+            icon="login"
+            :label="$t('in_game')"
+            @click="login"
+        >
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
