9ab76781eaebc5ad432dd7d8966c7908193d0194
dalinin-it <dalidim.it@gmail.com>
2021-02-13 23:41:38 +0300

    added messages

--- darquest/src/i18n/en-us/index.js
+++ darquest/src/i18n/en-us/index.js
@@ -1,3 +1,11 @@
 export default {
-  login: 'Login'
+  main: {
+    log_in: 'Log in',
+    login: 'Login',
+    in_game: 'In game',
+    password: 'Password'
+  },
+  app: {
+    name: 'DarQuest 1/18'
+  }
 }
--- darquest/src/i18n/ru/index.js
+++ darquest/src/i18n/ru/index.js
@@ -1,3 +1,11 @@
 export default {
-  login: 'Вход'
+  main: {
+    log_in: 'Войти',
+    login: 'Логин',
+    in_game: 'В игру',
+    password: 'Пароль'
+  },
+  app: {
+    name: 'DarQuest 1/18'
+  }
 }
