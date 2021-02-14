0da086cdd8e70bc1c4bae3f55390531f9a015930
dalinin-it <dalidim.it@gmail.com>
2021-02-14 23:16:25 +0300

    added tabs

--- darquest/src/components/app/TollbarProfileInfo.vue
+++ darquest/src/components/app/TollbarProfileInfo.vue
@@ -8,7 +8,22 @@
         @click="showLoginForm"
     >
       <div>
-        <login></login>
+        <q-tabs v-model="tabs">
+          <q-tab name="login" :label="$t('main.sign_in')"></q-tab>
+          <q-tab name="register" :label="$t('main.sign_up')"></q-tab>
+        </q-tabs>
+
+        <q-separator></q-separator>
+
+        <q-tab-panels v-model="tabs">
+          <q-tab-panel name="login">
+            <login></login>
+          </q-tab-panel>
+          <q-tab-panel name="register">
+            <register></register>
+          </q-tab-panel>
+        </q-tab-panels>
+
       </div>
     </q-btn-dropdown>
   </div>
@@ -16,10 +31,16 @@
 
 <script>
 import Login from '../auth/Login'
+import Register from 'components/auth/Register'
 
 export default {
   name: 'ToolbarProfileInfo',
-  components: { Login }
+  components: { Register, Login },
+  data () {
+    return {
+      tabs: 'login'
+    }
+  }
 }
 </script>
 
