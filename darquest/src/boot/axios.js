1047468ce2e5a48965e79d9e82df84d8e330c45f
dalinin-it <dalidim.it@gmail.com>
2021-02-17 21:18:52 +0300

    init Sanctum

--- darquest/src/boot/axios.js
+++ darquest/src/boot/axios.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
 Vue.prototype.$axios = axios
