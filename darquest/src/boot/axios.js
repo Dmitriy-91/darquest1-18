0a92bf319179282353a24ae3fbd349ddff0f819b
dalinin-it <dalidim.it@gmail.com>
2021-03-01 23:11:19 +0300

    added settings

--- darquest/src/boot/axios.js
+++ darquest/src/boot/axios.js
@@ -1,9 +1,19 @@
-import Vue from 'vue'
 import axios from 'axios'
+import Vue from 'vue'
 
 axios.defaults.withCredentials = true
+
+axios.defaults.baseURL = 'http://darquest.loc'
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
+axios.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
 Vue.prototype.$axios = axios
+
+export default axios
