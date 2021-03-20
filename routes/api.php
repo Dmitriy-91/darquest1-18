df6bc0145e2c9077b795b8c02f46aacf4f18879d
dalinin-it <dalidim.it@gmail.com>
2021-03-20 16:38:54 +0300

    added new route 'auth-user'

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function(){
+
+    /*
+     * Авторизованный пользователь
+     */
+    Route::get('auth-user', [AuthController::class, 'user'])
+        ->name( 'contact-info.show');
+
 });
