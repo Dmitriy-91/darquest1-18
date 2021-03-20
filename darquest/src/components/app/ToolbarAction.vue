e0060959316c104b31592672d49fe2c3744ce193
dalinin-it <dalidim.it@gmail.com>
2021-03-20 15:41:47 +0300

    init && took it out functionality auth and profile info to external components

---
+++ darquest/src/components/app/ToolbarAction.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <toolbar-auth-button v-show="!userIsLogin"></toolbar-auth-button>
+    <toolbar-profile-info v-show="userIsLogin"></toolbar-profile-info>
+  </div>
+</template>
+
+<script>
+import ToolbarAuthButton from './ToolbarAuthButton'
+import ToolbarProfileInfo from './ToolbarProfileInfo'
+
+export default {
+  name: 'ToolbarAction',
+  components: { ToolbarAuthButton, ToolbarProfileInfo },
+  computed: {
+    userIsLogin () {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
