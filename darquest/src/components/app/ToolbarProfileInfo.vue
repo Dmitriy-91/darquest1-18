e96c7b208e049f6794d2b7a5cc3396ee40e38ad1
dalinin-it <dalidim.it@gmail.com>
2021-03-20 15:40:31 +0300

    init

---
+++ darquest/src/components/app/ToolbarProfileInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-btn-dropdown
+      push
+      glossy
+      icon="account_circle"
+      :label="userName"
+  >
+    <div>
+      Profile
+    </div>
+  </q-btn-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'ToolbarProfileInfo',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    userIsLogin () {
+      return true
+    },
+    userName () {
+      return 'user name'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
