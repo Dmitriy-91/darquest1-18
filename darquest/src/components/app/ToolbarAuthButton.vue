b66e7b842343ba86280bf227924442c5e51280c4
dalinin-it <dalidim.it@gmail.com>
2021-03-20 15:40:24 +0300

    init

---
+++ darquest/src/components/app/ToolbarAuthButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-btn-dropdown
+      push
+      glossy
+      icon="login"
+      :label="$t('main.in_game')"
+  >
+    <div>
+      <q-tabs v-model="tabs">
+        <q-tab name="login" :label="$t('main.sign_in')"></q-tab>
+        <q-tab name="register" :label="$t('main.sign_up')"></q-tab>
+      </q-tabs>
+
+      <q-separator></q-separator>
+
+      <q-tab-panels v-model="tabs">
+        <q-tab-panel name="login">
+          <login v-show="!forgotPassword"
+                 @forgot-password="switchLoginForgotPassword"
+          ></login>
+          <forgotPassword
+              v-show="forgotPassword"
+              @back-to-login="switchLoginForgotPassword"
+          ></forgotPassword>
+        </q-tab-panel>
+        <q-tab-panel name="register">
+          <register></register>
+        </q-tab-panel>
+      </q-tab-panels>
+
+    </div>
+  </q-btn-dropdown>
+</template>
+
+<script>
+import Login from 'components/auth/Login'
+import Register from 'components/auth/Register'
+import ForgotPassword from 'components/auth/ForgotPassword'
+
+export default {
+  name: 'ToolbarAuthButton',
+  components: { Register, Login, ForgotPassword },
+  data () {
+    return {
+      tabs: 'login',
+      forgotPassword: false
+    }
+  },
+  methods: {
+    switchLoginForgotPassword () {
+      this.forgotPassword = !this.forgotPassword
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
