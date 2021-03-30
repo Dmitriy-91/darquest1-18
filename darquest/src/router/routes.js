8936ffa2ae76d36dae9d48f6bf882308e5fd74f9
dalinin-it <dalidim.it@gmail.com>
2021-03-30 19:10:22 +0300

    refactoring code

--- darquest/src/router/routes.js
+++ darquest/src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
+      {
+        path: '',
+        component: () => import('pages/Index.vue')
+      },
       {
         path: '/reset-password',
         component: () => import('pages/auth/ResetPassword.vue')
