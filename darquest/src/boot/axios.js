e6fc89878b049ad9dd71b05327a819b02c65142f
dalinin-it <dalidim.it@gmail.com>
2021-04-04 23:53:25 +0300

    added automatic scrf request

--- darquest/src/boot/axios.js
+++ darquest/src/boot/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import Vue from 'vue'
+import router from '../api/routes'
+import auth from '../api/auth'
+
+const csrfUri = router.auth['csrf-cookie']
 
 axios.defaults.withCredentials = true
 
@@ -8,9 +12,36 @@ axios.defaults.baseURL = 'http://darquest.loc'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(async function (config) {
+  const currentUri = config.url
+  // Если нет заголовка csrf token, то сделаем запрос на его получение
+  if (axios.defaults.headers.common['X-XSRF-TOKEN'] === undefined &&
+    currentUri !== csrfUri) {
+    await auth.csrf()
+  }
   return config
 }, function (error) {
+  if (
+    error.response &&
+    [401, 419].includes(error.response.status)
+    // &&
+    // store.getters["auth/authUser"] &&
+    // !store.getters["auth/guest"]
+  ) {
+    // store.dispatch("auth/logout");
+  }
+
+  console.log(error)
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  return response.data
+}, error => {
+  // if (error.response && error.response.data) {
+  //   // handle your errors here.
+  //   handleServerErrors(error.response.data)
+  // }
   return Promise.reject(error)
 })
 
